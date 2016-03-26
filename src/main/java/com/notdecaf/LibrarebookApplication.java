06a44228ffcb0ef7aaacd1813bb7e2829ff1aa6f
Aditya Balwani <adibalwani@gmail.com>
2016-03-26 19:14:21 -0400

    Added blank project

---
+++ src/main/java/com/notdecaf/LibrarebookApplication.java
@@ -0,0 +1,12 @@
+package com.notdecaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarebookApplication.class, args);
+	}
+}
