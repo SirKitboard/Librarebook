ca33484976e1e60dc7a0e9e8e5a41950ada6ff48
Aditya Balwani <adibalwani@gmail.com>
2016-03-28 04:41:19 -0400

    Added constructors to moderator and admin

--- src/main/java/com/notdecaf/models/Admin.java
+++ src/main/java/com/notdecaf/models/Admin.java
@@ -1,7 +1,10 @@
 package com.notdecaf.models;
 
+import com.notdecaf.helpers.PasswordStorage;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.util.Date;
 
 /**
  * Created by halaamenasy on 3/28/16.
@@ -9,4 +12,10 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("admin")
 public class Admin extends Moderator {
+    public Admin(String firstName, String lastName, Date dob, String email, String gender, String password) throws PasswordStorage.CannotPerformOperationException {
+        super(firstName, lastName, dob, email, gender, password);
+    }
+
+    public Admin() {
+    }
 }
