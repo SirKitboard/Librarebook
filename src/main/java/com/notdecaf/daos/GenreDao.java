7c28c67b9340af0ff6abaf5b1cdb7d0409674ebc
Aditya Balwani <adibalwani@gmail.com>
2016-03-28 02:22:18 -0400

    Added genre and genre dao

---
+++ src/main/java/com/notdecaf/daos/GenreDao.java
@@ -0,0 +1,13 @@
+//package com.notdecaf.daos;
+//
+//import com.notdecaf.models.Genre;
+//import org.springframework.data.repository.CrudRepository;
+//
+//import javax.transaction.Transactional;
+//
+///**
+// * Created by Adi on 3/28/2016.
+// */
+//@Transactional
+//public interface GenreDao extends CrudRepository<Genre, Long>{
+//}
---
+++ src/main/java/com/notdecaf/models/Genre.java
@@ -0,0 +1,45 @@
+package com.notdecaf.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+/**
+ * Created by Adi on 3/28/2016.
+ */
+@Entity
+@Table(name="genres")
+public class Genre {
+    public Genre() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private Set<Item> items;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+}
