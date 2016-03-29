6a0bc05dc3503a205dff78e49ec20402419edd2d
Aditya Balwani <adibalwani@gmail.com>
2016-03-28 22:29:05 -0400

    Added image helper

---
+++ src/main/java/com/notdecaf/helpers/Image.java
@@ -0,0 +1,12 @@
+package com.notdecaf.helpers;
+
+/**
+ * Created by Adi on 3/28/2016.
+ */
+public class Image {
+    private static final String DIRECTORY = "";
+
+    public static String toImage(String path) {
+        return DIRECTORY + path;
+    }
+}
