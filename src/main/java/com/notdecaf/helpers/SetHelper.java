af19515ccf632f22a2283edeb18e621445794d07
Aditya Balwani <adibalwani@gmail.com>
2016-03-31 15:59:35 -0400

    Added SetHelper Class

---
+++ src/main/java/com/notdecaf/helpers/SetHelper.java
@@ -0,0 +1,31 @@
+package com.notdecaf.helpers;
+
+import com.notdecaf.models.IDModel;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by adi on 31/3/16.
+ */
+public class SetHelper {
+    public static <E extends IDModel> boolean search(Set<E> set, E object) {
+        for(E item : set) {
+            if(item.getId() == object.getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static <E extends IDModel> Set<E> remove(Set<E> set, E object) {
+        Set<E> newSet = new HashSet<>();
+        for(E item : set) {
+            if(item.getId() == object.getId()) {
+                continue;
+            }
+            newSet.add(item);
+        }
+        return newSet;
+    }
+}
