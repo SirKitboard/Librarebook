e7f5299805962212eeb0f360ee8f703d6bbff07b
Halaa Menasy <menasyhalaa@gmail.com>
2016-05-02 16:25:58 -0400

    linked frontpage buttons

--- src/main/resources/static/js/components/homepage/loginSection.jsx
+++ src/main/resources/static/js/components/homepage/loginSection.jsx
@@ -6,16 +6,19 @@ define([
         getInitialState : function () {
             return null;
         },
+        showLogin: function() {
+            $("#modalLogin").openModal();
+        },
         render : function() {
             return (
                 <div className="section white" id={"section-"+this.props.id}>
                     <h2 className="section-heading">Welcome to LibrarEbook!</h2>
-                    
+
 
 					<h4 className="section-heading">Sign in to get access to our full site</h4>
 					<div className="login-buttons container row">
-                        <a className="waves-effect waves-light btn col s10 offset-s1 m4 offset-m1 l2 offset-l3">Login</a>
-						<a className="waves-effect waves-light btn col s10 offset-s1 m4 offset-m2 l2 offset-l2">Signup</a>
+                        <a onClick={this.showLogin} className="waves-effect waves-light btn col s10 offset-s1 m4 offset-m1 l2 offset-l3">Login</a>
+						<a onClick={this.showLogin} className="waves-effect waves-light btn col s10 offset-s1 m4 offset-m2 l2 offset-l2">Signup</a>
 					</div>
                 </div>
             )
