e261455870f1385ecec147c3ca37cb91c8061f31
Purav Shah <puravshah23@gmail.com>
2016-05-02 11:31:31 -0400

    Add click handler to book

--- src/main/resources/static/js/components/template/book.jsx
+++ src/main/resources/static/js/components/template/book.jsx
@@ -6,9 +6,11 @@ define([
         getInitialState: function(){
 			return null;
         },
+
         render: function() {
+			console.log(this.props.setUser);
 			return (
-				<div className="cart-book" style={{position:'relative', margin:'5px', padding:'5px'}}>
+				<div data-id={this.props.id} onClick={this.props.setUser} className="cart-book" style={{position:'relative', margin:'5px', padding:'5px'}}>
 					<img className="cover" src="http://placehold.it/75x75"/>
 					<i style={{position:'absolute', top:'3px', right:'3px'}}className="material-icons">close</i>
 					<span style={{position:'relative', top:'-62px', marginLeft:'5px'}}>{this.props.book.title ? this.props.book.title : "Placeholder"}</span>
